477f880156f1d490eba1522ed08d80346773a206
Nikolai Huckle <n.huckle@posteo.de>
2019-09-14 15:52:33 +0200

    Preparing to finish

--- testing.R
+++ testing.R
@@ -34,10 +34,14 @@ colnames(eloDatRe) <- stri_replace_all_fixed(colnames(eloDatRe), c(1, 2), c(2, 1
 eloList <- list(eloDat, eloDatRe)
 eloDat <- data.table::rbindlist(eloList, use.names = TRUE, idcol = TRUE)
 rm(eloList)
+# Replace the problematic New Orleans Hornets Season with NOH
+eloDat <- eloDat %>% mutate(team1 = ifelse((team1 == "NOP") & (season %in% 2003:2004), "NOH", team1))%>% 
+  mutate(team2 = ifelse((team2 == "NOP") & (season %in% 2003:2004), "NOH", team2))
+
 # Make conference dat
 confDat <- data.frame(team1 = sort(unique(eloDat$team1[eloDat$season >= 1977])), 
     conference = c(rep("East", 8), "West", "West", "East", "West", "West", "East", 
-        "West", rep("West", 3), "East", "East", "West", "East", "East", "West", "West", 
+        "West", rep("West", 3), "East", "East", "West", "East", "East","East", "West", "West", 
         "East", "East", "West", "East", "East", rep("West", 6), "East", "West", "West", 
         "East", "East"))
 
@@ -45,7 +49,15 @@ confDat <- data.frame(team1 = sort(unique(eloDat$team1[eloDat$season >= 1977])),
 tempElo <- eloDat %>% mutate(playoffgame = !is.na(playoff)) %>% group_by(season, 
     team1) %>% summarise_if(is.logical, sum) %>% right_join(eloDat, by = c("season", 
     "team1")) %>% mutate(playoffteam = playoffgame > 0) %>% filter(season >= 1977) %>% 
-    left_join(confDat, by = "team1") %>% mutate(playoffgame = !is.na(playoff))
+    left_join(confDat, by = "team1") %>% mutate(playoffgame = !is.na(playoff)) 
+
+
+#### Investigate carmElo weirdness ####
+tempElo %>% filter(season == 2019, is.na(playoff)) %>% group_by(team1) %>%
+  summarise_at(vars(carmelo1_pre, carmelo2_pre, playoffteam), mean, na.rm = TRUE) %>% left_join(confDat, by = "team1") %>%
+  ggplot(aes(x=carmelo1_pre, y = carmelo2_pre, colour = factor(playoffteam), label = team1))+geom_label()+
+  facet_wrap(~conference)+theme_dark()+theme(legend.position="none")
+
 
 #### Ridge-Plots ####
 
@@ -57,7 +69,7 @@ seasonData %>% filter(season >= 1980) %>%  ggplot( aes(x = elo2_pre, y = factor(
   geom_density_ridges_gradient(scale = 0.95) +
   scale_fill_viridis(name = "", option = "C")+
   xlab("Average Opponent Elo") + ylab("Seasons starting from ...") + theme_tufte(base_size = 15)+facet_wrap(~conference)+
-  theme(legend.position="none")
+  theme(legend.position="none")+scale_x_continuous(breaks = seq(1470,1530,by=20))
 
 ggplot( seasonData,aes(x=elo2_pre, y=conference, fill=factor(..quantile..))) +
   stat_density_ridges(scale = 0.95,
@@ -67,7 +79,53 @@ ggplot( seasonData,aes(x=elo2_pre, y=conference, fill=factor(..quantile..))) +
   scale_fill_viridis(discrete = TRUE, name = "Quartiles")+theme_tufte(base_size = 15)+xlab("Average Opponent Elo")+
   ylab("Conference")
 
+#### Unlucky lucky ####
 
+# Biggest Range in oppo-Elo per season & conference 
+luckyDat <- tempElo %>% filter(is.na(playoff)) %>% group_by(season, team1) %>% 
+  group_by(season, team1) %>% summarise_at(vars(elo1_pre, elo2_pre), mean) %>% ungroup() %>% left_join(confDat, by = "team1") %>%
+  group_by(season, conference) %>% summarise(oppoMax = max(elo2_pre),  oppoMin = min(elo2_pre)) %>% 
+  mutate(luck = oppoMax - oppoMin) %>% ungroup() %>% arrange(-luck) %>% mutate(index = paste0(season, conference))
+
+# Biggest 
+luckyDat2 <- tempElo %>% filter(is.na(playoff)) %>% group_by(season, team1) %>% 
+  group_by(season, team1) %>% summarise_at(vars(elo1_pre, elo2_pre), mean) %>% ungroup()  %>% left_join(confDat, by = "team1") %>%
+  group_by(season, conference) %>% mutate(elo1_z = as.numeric(scale(elo1_pre)), elo2_z = as.numeric(scale(elo2_pre))) %>% 
+  ungroup()%>% mutate(index = paste0(season, conference))
+
+# Biggest Range in z-Score Elo-Rating per season & conference
+luckyDat3 <- tempElo %>% filter(is.na(playoff)) %>% group_by(season, team1) %>% 
+  group_by(season, team1) %>% summarise_at(vars(elo1_pre, elo2_pre), mean) %>% ungroup()  %>% left_join(confDat, by = "team1") %>%
+  group_by(season, conference) %>% mutate(elo1_z = as.numeric(scale(elo1_pre)), elo2_z = as.numeric(scale(elo2_pre))) %>% 
+  ungroup() %>% group_by(season, conference) %>% summarise(oppoMax = max(elo2_z),  oppoMin = min(elo2_z)) %>% 
+  mutate(luck = oppoMax - oppoMin) %>% ungroup() %>% arrange(-luck) %>% mutate(index = paste0(season, conference))
+
+
+
+temp <- luckyDat2 %>% group_by(season, conference) %>% slice(which.min(elo2_z), which.max(elo2_z)) %>% 
+  mutate(kind = rep(c("min", "max"),n()/2))
+temp <- data.table::dcast(data.table::setDT(temp),season+conference~kind, value.var = c("elo2_z", "team1"))
+
+ggplot(data = temp, mapping = aes(x = season,group = factor(conference))) +
+  geom_linerange(mapping = aes(ymin =elo2_z_min,
+                                  ymax =  elo2_z_max)) +
+  labs(x= "Season", y= "Range of Deviation") + coord_flip()#+
+  facet_wrap(~conference)
+  
+ggplot(data = temp, aes(x = season, y = elo2_z_max-elo2_z_min, colour = conference)) +
+    labs(x= "Season", y= "Range of Deviation") +geom_line()+ylim(c(0,5))+xlab("Season")+ylab("Spread of Luck")
+ggplot(luckyDat2, aes(x = season, ymin = 0, ymax =luck))+geom_linerange()+facet_wrap(~conference)
+
+intersect(luckyDat$index[1:10], luckyDat3$index[1:10])
+
+selected <- tempElo %>% filter(season == 2012, is.na(playoff)) %>% 
+  filter(team1 %in% c("MIA", "CHI"))  %>% group_by(team1) %>% arrange(date, .by_group=TRUE) %>% ungroup() %>%
+  group_by(team1, team2) %>% mutate(wat = 1:n()) %>% ungroup() %>% mutate(team2rep = ifelse(team2 %in% c("MIA", "CHI"), "OPO", team2)) %>% 
+  mutate(matchup = paste0(team2rep, wat)) %>% add_count(matchup) 
+
+yas <- selected %>% filter(n> 1) %>% group_by(matchup) %>% summarise_at(vars(elo2_pre), diff) %>% ungroup() %>% arrange(-elo2_pre)
+
+#### random ####
 eloDelta <- tempElo %>% group_by(season, team1) %>% summarise(oppoMax = max(elo2_pre), 
     oppoMin = min(elo2_pre), ownMax = max(elo1_pre), ownMin = min(elo1_pre)) %>% 
     mutate(oppoDiff = oppoMax - oppoMin, ownDiff = ownMax - ownMin) %>% ungroup() %>% 
